8db3c34401421e4294ff013cdac2f6e155d5f6bd
khaliloualdchaib <oualdchaibkhalil@gmail.com>
2025-01-18 12:39:41 +0100

    finished loop

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate
 import os
 import shutil
-
+import pandas as pd
+from tqdm import tqdm
 
 def load_documents():
     document_loader = PyPDFLoader("data/Salesforce-CRM-User-Manual-St-Francis-St-Marks.pdf")
@@ -16,8 +17,8 @@ def load_documents():
 
 def split_documents(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=800,
-        chunk_overlap=80,
+        chunk_size=1200,
+        chunk_overlap=400,
         length_function=len
     )
     return text_splitter.split_documents(documents)
@@ -45,15 +46,23 @@ PROMPT_TEMPLATE = """
         """
 
 def main():
-    text = "how to create contact for a child"
-    results = db.similarity_search_with_relevance_scores(text, k=3)
-    if len(results) == 0 or results[0][1] < 0.7:
-        print(f'Unable to find matching results.')
-        return
-    context_text = "\n\n --------------\n\n".join([doc.page_content for doc, _score in results])
-    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    prompt = prompt_template.format(context=context_text, question=text)
-    llm = OllamaLLM(model="llama3.2")
-    result = llm.invoke(prompt)
-    print(result)    
+    df = pd.read_csv('Salesforce_Questions.csv')
+    data = {"Question": [], "Answer": []}
+    for index, row in tqdm(df.iterrows(), total=len(df), desc="Processing Questions"):
+        text = row["Question"]
+        data["Question"].append(text)
+        results = db.similarity_search_with_relevance_scores(text, k=3)
+        if len(results) == 0 or results[0][1] < 0.6:
+            data["Answer"].append('Unable to find matching results.')
+            print(f'Unable to find matching results.')
+        else:
+            context_text = "\n\n --------------\n\n".join([doc.page_content for doc, _score in results])
+            prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+            prompt = prompt_template.format(context=context_text, question=text)
+            llm = OllamaLLM(model="llama3.2")
+            result = llm.invoke(prompt)
+            data["Answer"].append(result)
+    res = pd.DataFrame(data)
+    res.to_csv("Salesforce_QA.csv")
+
 main()
